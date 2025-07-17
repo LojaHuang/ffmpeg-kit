5cda62eeb305cd39f153342a050d468eee5a046f
hlj <huanglongjin@xiaomai5.com>
2025-07-17 10:51:20 +0800

    add common-page-size

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -58,8 +58,10 @@ endif
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
     MY_LDFLAGS := -Wl,-z,max-page-size=16384
+    MY_LDFLAGS += -Wl,-z,common-page-size=16384
 else ifeq ($(TARGET_ARCH_ABI),x86_64)
     MY_LDFLAGS := -Wl,-z,max-page-size=16384
+    MY_LDFLAGS += -Wl,-z,common-page-size=16384
 endif
 
 include $(CLEAR_VARS)
